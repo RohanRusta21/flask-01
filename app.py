39defccc700e9485423ecd34d8cb44282c6daa91
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2023-05-14 01:19:49 +0530

    Created app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, abort
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    hostname = socket.gethostname()
+    html = f"<h1>Hello World!</h1> Served from <b>{hostname}</b>"
+    return html
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=3000)
